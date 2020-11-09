dbc2e1e37f67a0f46335c708ed3edf9f05a4340d
Julian Lara <l.julian99_71@hotmail.com>
2020-11-09 18:34:57 -0500

    Añadido admin

---
+++ Front-sercompras/Front-sercompras/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Front_sercompras.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Inicioadmin()
+        {
+            return View();
+        }
+        public ActionResult Gproveedores()
+        {
+            return View();
+        }
+        public ActionResult Gfuncionarios()
+        {
+            return View();
+        }
+    }
+}
